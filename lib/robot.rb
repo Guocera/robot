c1b04d54ff5337b28dcdb08ce1e810a7a7167c0b
Nick Yee <njsyee@gmail.com>
2016-01-14 11:32:51 -0700

    Robot can attack another robot if in range.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,16 +1,49 @@
 class Robot
-  attr_accessor :health_points
+  attr_accessor :health_points, :attack_power, :position
 
   class RobotAlreadyDeadError < StandardError;  end
   class UnattackableEnemyError < StandardError;  end
 
   def initialize
     @health_points = 100
+    @attack_power = 10
+    @position =[0,0]
+  end
+
+  def move_up
+    position[1] += 1
+  end
+
+  def move_down
+    position[1] -= 1
+  end
+
+  def move_left
+    position[0] -= 1
+  end
+
+  def move_right
+    position[0] += 1
+  end
+
+  def wound(dmg)
+    self.health_points -= dmg
+  end
+
+  def within_range(enemy)
+    ( (enemy.position[0] <= position[0] + 1) && (enemy.position[0] >= position[0] - 1) &&\
+      (enemy.position[1] <= position[1] + 1) && (enemy.position[1] >= position[1] - 1) )
+  end
+
+  def attack(enemy)
+    if within_range(enemy)
+      enemy.wound(self.attack_power)
+    end
   end
 
   def attack!(enemy)
     raise UnattackableEnemyError, 'Cannot attack non-Robots.' unless enemy.is_a? Robot
-    enemy.health_points -= 10
+    self.attack(enemy)
   end
 
   def heal(robot)
