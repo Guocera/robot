d51e334fdf7371637ae72e3f6bc650746040188e
Nick Yee <njsyee@gmail.com>
2016-01-14 10:06:47 -0700

    Robots can only attack robots.

--- lib/robot.rb
+++ lib/robot.rb
@@ -2,19 +2,28 @@ class Robot
   attr_accessor :health_points
 
   class RobotAlreadyDeadError < StandardError;  end
+  class UnattackableEnemyError < StandardError;  end
 
   def initialize
     @health_points = 100
   end
 
+  def attack!(enemy)
+    raise UnattackableEnemyError, 'Cannot attack non-Robots.' unless enemy.is_a? Robot
+    enemy.health_points -= 10
+  end
+
   def heal(robot)
+    raise RobotAlreadyDeadError, 'Robot is dead.' if self.health_points <= 0
     raise RobotAlreadyDeadError, 'Already at max health.' if robot.health_points <= 0
     robot.health_points += 10
   rescue RobotAlreadyDeadError => e
-    puts e.message
+    # puts e.message
   end
 
   def heal!
     heal(self)
   end
+
+
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,11 +2,24 @@ describe Robot do
   before do
     @robot = Robot.new
     @another_robot = Robot.new
+    @item = Item.new
   end
 
   it 'should have 100 HP' do
     expect(@robot.health_points).to eq(100)
   end
+
+  context '#attack!' do
+    it 'should do 10 dmg' do
+      @robot.attack!(@another_robot)
+      expect(@another_robot.health_points).to eq(90)
+    end
+
+    it 'should only be able to attack robots' do
+      expect{ @robot.attack!(@item) }.to raise_error StandardError
+    end
+  end
+
 
   context '#heal!' do
     it 'it should heal 10 hp' do
@@ -14,7 +27,7 @@ describe Robot do
       expect(@robot.health_points).to eq(110)
     end
 
-    context 'if 0 hp' do
+    context 'if robot has 0 hp' do
       it 'should not heal 10 hp' do
         @robot.health_points = 0
         @robot.heal!
@@ -26,10 +39,58 @@ describe Robot do
   end
 
   context '#heal' do
-    it 'it should heal other robot 10 hp' do
+    it 'should heal other robot 10 hp' do
       @robot.heal(@another_robot)
       expect(@another_robot.health_points).to eq(110)
       expect(@robot.health_points).to eq(100)
+    end
+
+    it 'should be healed by other robot by 10 hp' do
+      @another_robot.heal(@robot)
+      expect(@robot.health_points).to eq(110)
+      expect(@another_robot.health_points).to eq(100)
+    end
+
+    context 'if robot has 0 hp' do
+      before do
+        @robot.health_points = 0
+      end
+
+      context 'and if the other robot has hp' do
+        it 'should not heal the other robot 10 hp' do
+          @robot.heal(@another_robot)
+          expect(@another_robot.health_points).to eq(100)
+        end
+      end
+
+      context 'or if the other robot has 0 hp' do
+        it 'should not heal others 10 hp' do
+          @another_robot.health_points = 0
+          @robot.heal(@another_robot)
+          expect(@another_robot.health_points).to eq(0)
+        end
+      end
+    end
+
+    context 'if other robot has 0 hp' do
+      before do
+        @another_robot.health_points = 0
+      end
+
+      context 'and if the robot has hp' do
+        it 'should not heal others 10 hp' do
+          @robot.heal(@another_robot)
+          expect(@another_robot.health_points).to eq(0)
+        end
+      end
+
+      context 'or if the robot has 0 hp' do
+        it 'should not heal others 10 hp' do
+          @robot.health_points = 0
+          @robot.heal(@another_robot)
+          expect(@another_robot.health_points).to eq(0)
+        end
+      end
     end
   end
 end
