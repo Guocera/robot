1c9571f5ed9aaa7274fd4c1cac5562c641afde71
Nick Yee <njsyee@gmail.com>
2016-01-14 09:09:41 -0700

    Robot can heal others and themselves.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,3 +1,20 @@
 class Robot
+  attr_accessor :health_points
 
+  class RobotAlreadyDeadError < StandardError;  end
+
+  def initialize
+    @health_points = 100
+  end
+
+  def heal(robot)
+    raise RobotAlreadyDeadError, 'Already at max health.' if robot.health_points <= 0
+    robot.health_points += 10
+  rescue RobotAlreadyDeadError => e
+    puts e.message
+  end
+
+  def heal!
+    heal(self)
+  end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,3 +1,35 @@
 describe Robot do
+  before do
+    @robot = Robot.new
+    @another_robot = Robot.new
+  end
 
+  it 'should have 100 HP' do
+    expect(@robot.health_points).to eq(100)
+  end
+
+  context '#heal!' do
+    it 'it should heal 10 hp' do
+      @robot.heal!
+      expect(@robot.health_points).to eq(110)
+    end
+
+    context 'if 0 hp' do
+      it 'should not heal 10 hp' do
+        @robot.health_points = 0
+        @robot.heal!
+        expect(@robot.health_points).to eq(0)
+      end
+    end
+      
+  
+  end
+
+  context '#heal' do
+    it 'it should heal other robot 10 hp' do
+      @robot.heal(@another_robot)
+      expect(@another_robot.health_points).to eq(110)
+      expect(@robot.health_points).to eq(100)
+    end
+  end
 end
